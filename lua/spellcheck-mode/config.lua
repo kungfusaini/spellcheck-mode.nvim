f2a2453bbbda9430f4a2e03a2968080c509108ee
sumeet <sumeetsaini621@gmail.com>
2025-09-06 14:26:41 -0400

    better modularisation

--- lua/spellcheck-mode/config.lua
+++ lua/spellcheck-mode/config.lua
@@ -2,18 +2,17 @@ local M = {}
 
 -- Default configuration
 local defaults = {
-  keys = {
-    toggle = '<leader>sp',
-    next_error = 'n',
-    prev_error = 'p',
-    suggestions = '<Space>'
-  },
-  options = {
-    default_lang = 'en_gb',
-    max_suggestions = 10,
-    auto_enable_filetypes = { 'markdown', 'gitcommit', 'text', 'tex' },
-    spell_options = 'camel'
-  }
+	keys = {
+		next_error = 'n',
+		prev_error = 'p',
+		suggestions = '<Space>'
+	},
+	options = {
+		default_lang = 'en_gb',
+		max_suggestions = 10,
+		auto_enable_filetypes = { 'markdown', 'gitcommit', 'text', 'tex' },
+		spell_options = 'camel'
+	}
 }
 
 -- Current configuration
@@ -21,28 +20,29 @@ M.current = vim.deepcopy(defaults)
 
 -- Merge user configuration with defaults
 function M.set(user_config)
-  user_config = user_config or {}
-  
-  -- Merge keys
-  if user_config.keys then
-    M.current.keys = vim.tbl_extend('force', M.current.keys, user_config.keys)
-  end
-  
-  -- Merge options
-  if user_config.options then
-    M.current.options = vim.tbl_extend('force', M.current.options, user_config.options)
-  end
-  
-  -- Apply spell options
-  vim.opt.spelloptions = M.current.options.spell_options
+	user_config = user_config or {}
+
+	-- Merge keys
+	if user_config.keys then
+		M.current.keys = vim.tbl_extend('force', M.current.keys, user_config.keys)
+	end
+
+	-- Merge options
+	if user_config.options then
+		M.current.options = vim.tbl_extend('force', M.current.options, user_config.options)
+	end
+
+	-- Apply spell options
+	vim.opt.spelloptions = M.current.options.spell_options
 end
 
 -- Get configuration
 function M.get()
-  return M.current
+	return M.current
 end
 
 -- Initialize with defaults
 M.set()
 
 return M
+
